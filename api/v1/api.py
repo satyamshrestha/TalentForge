4830262a9f8d04696bc4e30419b4085c94f7fa36
Satyam Shrestha <shresthasatyam7@gmail.com>
2026-06-07 16:26:54 +0900

    Resume Endpoints Included

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from routers import auth_router
+from routers import auth_router, resume_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router.router)
+api_router.include_router(resume_router.router)
