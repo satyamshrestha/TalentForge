d3813951b2d34abc33142cbc0fc63badafa94fce
Satyam Shrestha <shresthasatyam7@gmail.com>
2026-06-11 21:36:41 +0900

    Interview Repository Added

---
+++ repositories/interview_repository.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+
+from models.interview import Interview
+from models.question import Question
+
+class InterviewRepository():
+
+    def create_interview(
+        self,
+        db: Session,
+        interview: Interview
+    ):
+        db.add(interview)
+        db.commit()
+        db.refresh(interview)
+        return interview
+
+    def create_question(
+        self,
+        db: Session,
+        question: Question
+    ):
+        db.add(question)
+        db.commit()
+        db.refresh(question)
+        return question
+
+    def get_interview_by_id(
+        self,
+        db: Session,
+        id: str
+    ):
+        return (db.query(Interview).filter(Interview.id == id).first())
