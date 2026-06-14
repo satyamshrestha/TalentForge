0bca2347eab81b05ae0e2b3dc303e09e4522f705
Satyam Shrestha <shresthasatyam7@gmail.com>
2026-06-14 12:51:48 +0900

    feat(dashboard): created dashboard repository

---
+++ repositories/dashboard_repository.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+
+from models.resume import Resume
+from models.interview import Interview
+
+class DashboardRepository:
+    def get_total_resumes(
+        self,
+        db: Session,
+        user_id: str
+    ):
+        return (db.query(Resume).filter(Resume.user_id==user_id).count())
+    
+    def get_total_interviews(
+        self,
+        db: Session,
+        user_id: str
+    ):
+        return (db.query(Interview).filter(Interview.user_id==user_id).count())
+    
+    def get_completed_interviews(
+        self,
+        db: Session,
+        user_id: str
+    ):
+        return (
+            db.query(Interview)
+            .filter(
+                Interview.user_id==user_id,
+                Interview.status=="COMPLETED"
+            )
+            .count()
+        )
