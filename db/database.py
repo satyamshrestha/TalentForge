bcc8c309f1f555f6b5c70e5c81e0a54876f2a0f2
Satyam Shrestha <shresthasatyam7@gmail.com>
2026-08-10 17:04:39 +0900

    feat: harden database and redis connections

--- db/database.py
+++ db/database.py
@@ -3,11 +3,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from utils.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 Base = declarative_base()
--- db/redis.py
+++ db/redis.py
@@ -1,7 +1,10 @@
 from redis import Redis
+
 from utils.config import settings
 
 redis_client = Redis.from_url(
     settings.REDIS_URL,
-    decode_responses=True
+    decode_responses=True,
+    socket_connect_timeout=5,
+    socket_timeout=5,
 )
